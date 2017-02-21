d51d8559c66d43a61506a13bca8aa4a544669662
876038559@qq.com <112358>
2017-02-21 21:17:13 +0800

    Merge branch 'master' of H:\JavaProject\ThinkInJava with conflicts.

---
+++ src/forteen/BoundedClassReference.java
@@ -0,0 +1,7 @@
+package forteen;
+
+/**
+ * Created by Administrator on 2017/2/19.
+ */
+public class BoundedClassReference {
+}
---
+++ src/forteen/ClassInitialization.java
@@ -0,0 +1,7 @@
+package forteen;
+
+/**
+ * Created by Administrator on 2017/2/18.
+ */
+public class ClassInitialization {
+}
